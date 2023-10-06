47fef5e914058059ed965fa1bd540afc9411a77f
A-Alex-ander <Dubrov.work@gmail.com>
2023-10-06 15:13:46 +0700

    commit html-css week 4

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Icon} from "../../components/icon/Icon";
+import {FlexWrapper} from "../../components/FlexWrapper";
+import {S} from './Footer_Styles'
+
+
+const socialItemsData = [
+    {
+        iconId: "linkedin"
+    },
+    {
+        iconId: "vk"
+    },
+    {
+        iconId: "telegram"
+    }
+
+]
+export const Footer: React.FC = () => {
+    return (
+        <S.Footer>
+            <FlexWrapper direction={"column"} align={"center"}>
+                <S.Name>Alexander</S.Name>
+                <S.SocialList>
+
+                    {socialItemsData.map((s, index) => {
+                        return (
+                            <S.SocialItem key={index}>
+                                <S.SocialLink>
+                                    <Icon height={"21"} width={"21"} vieBox={"0 0 21 21"} iconId={s.iconId}/>
+                                </S.SocialLink>
+                            </S.SocialItem>
+                        )
+                    })}
+
+                </S.SocialList>
+                <S.Copyright>© 2023 Alexander Dubrov, All Rights Reserved.</S.Copyright>
+            </FlexWrapper>
+        </S.Footer>
+    );
+};
---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from "styled-components";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Icon} from "../../../components/icon/Icon";
+import {Slider} from "../../../components/slider/Slider";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/container/Container";
+import {S} from './../skills/Skills_Styles'
+export const Testimony: React.FC = () => {
+    return (
+        <StyledTestimony id={"testimony"} >
+            <Container>
+                <SectionTitle>Testimony</SectionTitle>
+
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <S.IconWrapper>
+                        <Icon iconId={"quote"}/>
+                    </S.IconWrapper>
+                    <Slider/>
+                </FlexWrapper>
+            </Container>
+
+
+        </StyledTestimony>
+    );
+};
+
+const StyledTestimony = styled.section`
+  min-height: 50vh;
+  background-color: #fde0ff;
+  position: relative;
+
+  ${S.IconWrapper} {
+    margin: 40px 0 72px;
+  }
+`
+
